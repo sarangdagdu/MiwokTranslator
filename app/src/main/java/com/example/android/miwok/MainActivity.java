5d66f7be04eda45776ca13ca2d3fa1e51f785dd8
Sarang Dagdu <sarang.dagdu@outlook.com>
2021-06-23 10:24:57 +0530

    Added List of Words
    Added List of English Words in the NumbersActivity. Also added log statements to verify the list is populated correctly.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,21 +18,75 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView textViewNumbers;
+    TextView textViewColors;
+    TextView textViewPhrases;
+    TextView textViewFamilyMembers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        textViewNumbers = findViewById(R.id.numbers);
+        textViewNumbers.setOnClickListener(this);
+        textViewPhrases = findViewById(R.id.phrases);
+        textViewPhrases.setOnClickListener(this);
+        textViewColors = findViewById(R.id.colors);
+        textViewColors.setOnClickListener(this);
+        textViewFamilyMembers = findViewById(R.id.family);
+        textViewFamilyMembers.setOnClickListener(this);
+
     }
 
-    public void openNumbersList(View view){
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.numbers){
+            tapNumbersTextView();
+        }
+        else if(v.getId() == R.id.colors){
+            tapColorsTextView();
+        }
+        else if(v.getId() == R.id.phrases){
+            tapPhrasesTextView();
+        }
+        else if(v.getId() == R.id.family){
+            tapFamilyMembersTextView();
+        }
+    }
+
+    private void tapFamilyMembersTextView() {
+        Intent intent = new Intent(getApplicationContext(), FamilyActivity.class);
+        startActivity(intent);
+        Toast.makeText(this,"Opened Family Members Activity",Toast.LENGTH_LONG).show();
+
+    }
+
+    private void tapPhrasesTextView() {
+        Intent intent = new Intent(getApplicationContext(), PhrasesActivity.class);
+        startActivity(intent);
+        Toast.makeText(this,"Opened Phrases Activity",Toast.LENGTH_LONG).show();
+
+    }
+
+    private void tapColorsTextView() {
+        Intent intent = new Intent(getApplicationContext(), ColorsActivity.class);
+        startActivity(intent);
+        Toast.makeText(this,"Opened Colors Activity",Toast.LENGTH_LONG).show();
+
+    }
+
+    private void tapNumbersTextView() {
         Intent intent = new Intent(getApplicationContext(), NumbersActivity.class);
         startActivity(intent);
+        Toast.makeText(this,"Opened Numbers Activity",Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,12 +3,33 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    ArrayList<String> englishWordsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        englishWordsList = new ArrayList<String>();
+        
+        englishWordsList.add("one");
+        englishWordsList.add("two");
+        englishWordsList.add("three");
+        englishWordsList.add("four");
+        englishWordsList.add("five");
+        englishWordsList.add("six");
+        englishWordsList.add("seven");
+        englishWordsList.add("eight");
+        englishWordsList.add("nine");
+        englishWordsList.add("ten");
+
+        for (int i = 0; i < englishWordsList.size(); i++) {
+            Log.v("NumbersActivity", "Word at index " + i + ": " + englishWordsList.get(i));
+        }
     }
 }
